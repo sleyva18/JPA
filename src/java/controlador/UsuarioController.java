147857cf4daf8a98cee97bbc46599b890eef25c8
sleyva18 <sleyva@vallegrande.edu.pe>
2018-12-14 19:32:41 -0500

    iniciar sesion implementado

---
+++ src/java/controlador/UsuarioController.java
@@ -0,0 +1,79 @@
+package controlador;
+
+import modelo.entidad.Usuario;
+import modelo.dao.UsuarioFacade;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named("usuarioController")
+@SessionScoped
+public class UsuarioController implements Serializable {
+
+    @EJB
+    private modelo.dao.UsuarioFacade ejbFacade;
+    private List<Usuario> items = null;
+    private Usuario selected;
+    private String usuario, contra;
+
+    public String validar() throws Exception {
+        FacesContext contex = FacesContext.getCurrentInstance();
+        Usuario u = ejbFacade.validarUsuario(usuario,contra);
+        if (u != null) {
+            contex.getExternalContext().redirect("faces/index.xhtml");
+            contex.addMessage(null, new FacesMessage("Bienvenido" + u.getUsuusu()));
+
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("usuario o contraseña incorrecta"));
+            return "login";
+        }
+        contex.addMessage(null, new FacesMessage("error"));
+        return null;
+    }
+
+    public UsuarioFacade getEjbFacade() {
+        return ejbFacade;
+    }
+
+    public void setEjbFacade(UsuarioFacade ejbFacade) {
+        this.ejbFacade = ejbFacade;
+    }
+
+    public List<Usuario> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Usuario> items) {
+        this.items = items;
+    }
+
+    public Usuario getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Usuario selected) {
+        this.selected = selected;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+}
---
+++ src/java/modelo/dao/UsuarioFacade.java
@@ -0,0 +1,42 @@
+package modelo.dao;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import modelo.entidad.Usuario;
+
+/**
+ *
+ * @author Administrador
+ */
+@Stateless
+public class UsuarioFacade extends AbstractFacade<Usuario> {
+
+    @PersistenceContext(unitName = "PoolNotasPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UsuarioFacade() {
+        super(Usuario.class);
+    }
+
+    public Usuario validarUsuario(String usuario,String contra) {
+        Usuario user = null;
+        String jpql = "SELECT u FROM Usuario u WHERE "
+                + "u.usuusu = :usuusu and u.pasusu = :pasusu";
+        Query p = em.createQuery(jpql);
+        p.setParameter("usuusu", usuario);
+        p.setParameter("pasusu", contra);
+        List<Usuario> lstUsuario = p.getResultList();
+        if (lstUsuario.size() > 0) {
+            user = lstUsuario.get(0);
+        }
+        return user;
+    }
+}
